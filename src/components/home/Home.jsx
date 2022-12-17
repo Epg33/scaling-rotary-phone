38b1cbffd78d50efd853761d362f9332ea8b1dc9
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-12-17 18:06:38 -0500

    global styles

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './home.module.css'
+
+const Home = () => {
+  return (
+    <main className={style.main}>
+      <h1>Home...</h1>
+    </main>
+  )
+}
+
+export default Home
