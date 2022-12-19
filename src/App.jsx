0da36b4e7456af8ddff71127637c66e16cc53f1c
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-12-19 13:05:25 -0500

    planeacion de proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/home/Home.jsx'
+import Nav from './components/navbar/Nav.jsx'
 
 function App() {
   return (
     <Router>
+      <Nav />
       <Routes>
         <Route path='/' element={<Home />}></Route>
       </Routes>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import {useEffect} from 'react'
+import axios from 'axios'
 import style from './home.module.css'
 
 const Home = () => {
+  useEffect(() => {
+    axios.get('https://api.themoviedb.org/3/discover/movie?api_key=5433a58ed58a7253f675b66bb885524d&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate')
+      .then(res=> console.log(res))
+  }, [])
+  
   return (
     <main className={style.main}>
       <h1>Home...</h1>
