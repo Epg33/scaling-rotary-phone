5aaf3005af2935aa6f350f0d9c16f8f570f17d79
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-12-20 13:56:48 -0500

    quitar collections y hacer fetching

--- src/components/collections/Collections.jsx
+++ src/components/collections/Collections.jsx
@@ -1,6 +1,14 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import style from './collections.module.css'
 
 function Collections() {
+  const [collections, setCollections] = useState()
+  useEffect(()=>{
+    axios.get('https://api.themoviedb.org/3/collection/10?api_key=5433a58ed58a7253f675b66bb885524d&language=en-US')
+      .then(res => console.log(res))
+  }, [])
+  
   return (
     <div>Collections</div>
   )
--- src/components/genres/Genres.jsx
+++ src/components/genres/Genres.jsx
@@ -1,6 +1,13 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import style from './genres.module.css'
 
 function Genres() {
+  useEffect(()=>{
+    axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=5433a58ed58a7253f675b66bb885524d&language=en-US')
+      .then(res => console.log(res))
+      
+  })
   return (
     <div>Genres</div>
   )
