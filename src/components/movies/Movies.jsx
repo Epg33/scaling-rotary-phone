ff30373fa094d07d484cd14461ebdac21c3a711b
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-12-24 13:14:50 -0500

    acomodar

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -1,30 +1,45 @@
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import {AiFillStar} from 'react-icons/ai'
 import style from './movies.module.css'
 import Loading from '../loading/Loading'
 
 function Movies() {
   const [content, setContent] = useState();
+  const [numPage, setNumPage] = useState(1)
+  const container = useRef()
   
-  const fetchingMovies = async () => {
-    const res = await axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=5433a58ed58a7253f675b66bb885524d&language=en-US&page=1')
+  const fetchingMovies = async (page) => {
+    const res = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=5433a58ed58a7253f675b66bb885524d&language=en-US&page=${page}`)
     const data = await res.data.results;
     console.log(data);
-    setContent(data);
+    if(!content) setContent(data);
+    else setContent((cont)=>[...cont, data]);
   }
   
   useEffect(()=>{
-    fetchingMovies()
-  }, [])
+    fetchingMovies(numPage)
+    console.log(numPage)
+  }, [numPage])
+
+  useEffect(()=>{
+    const change = event => {
+      if(window.scrollY> container.current.offsetHeight*0.85){
+        setNumPage(numPage+2)
+        console.log(container)
+      }
+    }
+    window.addEventListener('scroll', change)
+    return ()=>{ window.removeEventListener('scroll', change)}
+  },[])
 
   if(!content) return <Loading />
   return (
-    <div>
+    <div ref={container}>
       {
-        content.map((movie)=>{
+        content.map((movie, index)=>{
           return <div key={movie.id}>
-            <h3>{movie.title}</h3>
+            <h3>{index+1}. {movie.title}</h3>
             <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face${movie.backdrop_path}`} />
             <span><AiFillStar/> {movie.vote_average}/10</span>
           </div>
