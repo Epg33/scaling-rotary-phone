b5daed5e2061d1b9eef1bcb1d30260a47471fa5e
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-12-27 18:03:30 -0500

    implementar intersectionObserver y implementar correctamente el scroll infinito

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -8,6 +8,10 @@ function Movies() {
   const [content, setContent] = useState();
   const [numPage, setNumPage] = useState(1)
   const container = useRef()
+
+
+
+  const observer = new IntersectionObserver()
   
   const fetchingMovies = async (page) => {
     const res = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=5433a58ed58a7253f675b66bb885524d&language=en-US&page=${page}`)
@@ -19,26 +23,36 @@ function Movies() {
   
   useEffect(()=>{
     fetchingMovies(numPage)
-    console.log(numPage)
+    console.log(container)
   }, [numPage])
 
   useEffect(()=>{
     const change = event => {
-      if(window.scrollY> container.current.offsetHeight*0.85){
+      if(window.scrollY> container.current.offsetHeight*0.65){
         setNumPage(numPage+1)
         console.log(content)
       }
     }
     window.addEventListener('scroll', change)
     return ()=>{ window.removeEventListener('scroll', change)}
-  },[])
+  }, [])
+
+  // setTimeout(()=>{
+  //   console.log(container.current.parentElement)
+  //   console.log(container.current.offsetHeight);
+  //   console.log(container);
+  // },5000)
+
+  const login = () => {
+    console.log('visto')
+  }
 
   if(!content) return <Loading />
   return (
     <div className={style.movies_container} ref={container}>
       {
         content.map((movie, index)=>{
-          return <div key={index} className={style.movie}>
+          return <div key={index} className={style.movie} onFocus={()=>index === content.length-1 ? login() : null}>
             <h3 className={style.title}>{index+1}. {movie.title}</h3>
             <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`} />
             <p>{movie.overview}</p>
