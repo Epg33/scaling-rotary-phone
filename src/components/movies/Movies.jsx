afbf95bd6c4d76538d1d1d72fa87897a33d8cdf3
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-12-30 13:52:12 -0500

    corregir intersection observer

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -1,10 +1,11 @@
 import InfiniteScroll from 'react-infinite-scroll-component'
 import axios from 'axios'
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useState } from 'react'
 import {AiFillStar} from 'react-icons/ai'
 import {BsCameraReelsFill} from 'react-icons/bs'
 import style from './movies.module.css'
 import Loading from '../loading/Loading'
+import { InView } from 'react-intersection-observer'
 
 function Movies() {
   const [content, setContent] = useState();
@@ -15,40 +16,44 @@ function Movies() {
     const data = await res.data.results;
     console.log(data);
     if(!content) return await  data;
-    return await [...content, data[0]]
+    return [...content, data[0]]
   }
 
-  const trying = async () => {
-    const res = await fetchingMovies(numPage)
-    // setContent(()=>fetchingMovies(numPage))
-    console.log(res)
-    console.log(numPage);
-  }
-  
-  useEffect(async()=>{
-    trying()
+  useEffect(()=>{
+    const trying = async () => {
+      const res = await fetchingMovies(numPage)
+      setContent(res)
+      console.log(content);
+      console.log(numPage);
+    }
+    trying()  
   }, [numPage])
 
   if(!content) return <Loading />
   return (
-    <InfiniteScroll 
-    dataLength={content.length}
-    next={()=>{setNumPage(numPage+1);fetchingMovies}}
-    hasMore={numPage<500}
-    className={style.movies_container}
-    loader={<Loading />}>
-      {
-        content.map((movie, index)=>{
-          return <div key={index} className={style.movie}>
-            <h3 className={style.title}>{index+1}. {movie.title}</h3>
-            {movie.poster_path ? <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`} /> 
-            : <BsCameraReelsFill className={style.default}/>}
-            <p>{movie.overview}</p>
-            <span><AiFillStar/> {movie.vote_average}/10</span>
-          </div>
-        })
-      }
-    </InfiniteScroll>
+    <section style={{display: 'flex', flexDirection: 'column'}}>
+      {/* <InfiniteScroll  
+      // dataLength={content.length}
+      // next={()=>{fetchingMovies}}
+      // hasMore={numPage<500}
+      // className={style.movies_container}
+      // loader={<Loading />}>*/}
+        {
+          content.map((movie, index)=>{
+            return <div key={index} className={style.movie}>
+              <h3 className={style.title}>{index+1}. {movie.title}</h3>
+              {movie.poster_path ? <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`} /> 
+              : <BsCameraReelsFill className={style.default}/>}
+              <p>{movie.overview}</p>
+              <span><AiFillStar/> {movie.vote_average}/10</span>
+            </div>
+          })
+        }
+      {/* </InfiniteScroll> */}
+      <InView as='div' onChange={(inView, entry)=>{inView ? setNumPage(numPage+1) : null;console.log(inView, entry)}}>
+        <footer>footer</footer>
+      </InView>
+    </section>
   )
 }
 
