1c421ce23684f633fd604b2735aaca09917f48cb
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-12-31 16:04:06 -0500

    terminado scroll infinito, acomodar estilos

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -1,4 +1,3 @@
-import InfiniteScroll from 'react-infinite-scroll-component'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import {AiFillStar} from 'react-icons/ai'
@@ -14,17 +13,14 @@ function Movies() {
   const fetchingMovies = async (page) => {
     const res = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=5433a58ed58a7253f675b66bb885524d&language=en-US&page=${page}`)
     const data = await res.data.results;
-    console.log(data);
     if(!content) return await  data;
-    return [...content, data[0]]
+    return [...content, data]
   }
 
   useEffect(()=>{
     const trying = async () => {
       const res = await fetchingMovies(numPage)
-      setContent(res)
-      console.log(content);
-      console.log(numPage);
+      setContent([...res])
     }
     trying()  
   }, [numPage])
@@ -32,16 +28,21 @@ function Movies() {
   if(!content) return <Loading />
   return (
     <section style={{display: 'flex', flexDirection: 'column'}}>
-      {/* <InfiniteScroll  
-      // dataLength={content.length}
-      // next={()=>{fetchingMovies}}
-      // hasMore={numPage<500}
-      // className={style.movies_container}
-      // loader={<Loading />}>*/}
         {
           content.map((movie, index)=>{
+            if(index>19){
+              return movie.map((peli, index)=>{
+                return <div key={index} className={style.movie}>
+                  <h3 className={style.title}>{peli.title}</h3>
+                  {peli.poster_path ? <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face${peli.poster_path}`} /> 
+                  : <BsCameraReelsFill className={style.default}/>}
+                  <p>{peli.overview}</p>
+                  <span><AiFillStar/> {peli.vote_average}/10</span>
+                </div>
+              })
+            }
             return <div key={index} className={style.movie}>
-              <h3 className={style.title}>{index+1}. {movie.title}</h3>
+              <h3 className={style.title}>{movie.title}</h3>
               {movie.poster_path ? <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`} /> 
               : <BsCameraReelsFill className={style.default}/>}
               <p>{movie.overview}</p>
@@ -49,7 +50,6 @@ function Movies() {
             </div>
           })
         }
-      {/* </InfiniteScroll> */}
       <InView as='div' onChange={(inView, entry)=>{inView ? setNumPage(numPage+1) : null;console.log(inView, entry)}}>
         <footer>footer</footer>
       </InView>
